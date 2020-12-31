36a51e91360f71a7c2937abf837ff95c247b0228
rades12340 <rades12340@gmail.com>
2020-12-31 23:51:12 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,97 @@
+import "../styles.css"
+
+const elements = document.querySelectorAll('button')! as NodeListOf<HTMLButtonElement>;
+let display = (document.getElementById('inp')! as HTMLInputElement)
+
+const initvalue = 0
+
+display.value = initvalue.toString()
+
+let total: number = 0;
+let operator: string = '';
+let number: string = '';
+
+
+elements.forEach(el => el.addEventListener('click', (e: any) => {
+    if (isNaN(+e.target.innerText)) {
+        let value = e.target.innerText;
+
+        if (value === 'AC') {
+            display.value = initvalue.toString()
+            total = 0
+            number = ''
+            operator = ''
+        }
+        if (value === "+/-") {
+            total = +total < 0 ? total : total
+        }
+        if (value === "%") {
+            display.value = "%"
+            total = +total * 0.01
+        }
+
+        if (value === '.') {
+            number += '.'
+        }
+
+        if (value === "/") {
+            display.value = "/"
+            operator = '/'
+            if (total === 0) {
+                total += +number
+            }
+            number = ''
+        }
+
+        if (value === "*") {
+            display.value = "*"
+            operator = '*'
+            if (total === 0) {
+                total += +number
+            }
+            number = ''
+        }
+
+        if (value === "-") {
+            display.value = "-"
+            operator = '-'
+            if (total === 0) {
+                total += +number
+            }
+            number = ''
+        }
+
+        if (value === "+") {
+            display.value = "+"
+            operator = '+'
+            total += +number
+            number = ''
+        }
+
+        if (value === "=") {
+            if (operator === '/') {
+                total /= +number
+                number = ''
+            }
+            if (operator === '*') {
+                total *= +number
+                number = ''
+            }
+            if (operator === '-') {
+                total -= +number
+                number = ''
+            }
+            if (operator === '+') {
+                total += +number
+                number = ''
+            }
+            display.value = total.toString()
+        }
+    } else {
+        number += e.target.innerText
+        display.value = number.toString()
+    }
+}))
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/app.ts"],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: true }),
+    new HtmlWebpackPlugin({
+      favicon: "./favicon.ico",
+      template: "./index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".csv"],
+  },
+};
